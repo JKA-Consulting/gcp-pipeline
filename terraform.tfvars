719996c7915ecb8ad60748b9343a5e7145d31f91
Abasi-ifreke <uwemabasifrekelive@gmail.com>
2025-01-27 16:09:14 +0100

    modified the tf config and cloudbuild

---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+region = "us-central1"
+
+subnet-cidr = "10.0.0.0/16"
+
+subnet-region = "us-central1"
+
+subnet-zone = "us-central1-a"
--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
 variable "region" {
   description = "default project region"
-  default = "us-central1"
 }
 
 variable "project-id" {
@@ -9,15 +8,12 @@ variable "project-id" {
 
 variable "subnet-cidr" {
   description = "project cidr block"
-  default = "10.0.0.0/16"
 }
 
 variable "subnet-region" {
   description = "subnet region of the vpc"
-  default = "us-central1"
 }
 
 variable "subnet-zone" {
   description = "subnet zone of the vpc"
-  default = "us-central1-a"
 }
